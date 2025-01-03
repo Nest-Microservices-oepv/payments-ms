e66af1072fc48cf748e147a33e5d7d19261f7f26
oscarepv <oscar_e1993@hotmail.com>
2025-01-03 16:45:34 -0500

    add NATS microservice integration and update environment variables

--- src/pyments/pyments.module.ts
+++ src/pyments/pyments.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PaymentsService } from './pyments.service';
 import { PaymentsController } from './pyments.controller';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [PaymentsController],
   providers: [PaymentsService],
+  imports: [NatsModule],
 })
 export class PaymentsModule {}
